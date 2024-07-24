a71f442faa9aac05ed2b042c564ef2986eb71c49
Lautaro Bustos <uri21199@gmail.com>
2024-07-24 10:45:07 -0300

    aprobacion

--- src/routes/resolucion_sol.py
+++ src/routes/resolucion_sol.py
@@ -10,25 +10,26 @@ def aprobar_solicitud():
         empleado = session['empleado']
         roles = session['roles']
         personal = session['personal']
+
         if request.method == 'POST':
-            # Obtener los datos del formulario
-            empleado_sol = request.form.get('empleado')
-            fecha = request.form.get('fecha')
-            concepto = request.form.get('concepto')
-            if concepto == 'Home Office':
-                concepto = 'dia_home'
-            elif concepto == 'Día de estudio':
-                concepto = "dia_estudio"
-            elif concepto == 'Vacaciones':
-                concepto = "dia_vacaciones"
-            elif concepto == 'Ausencia':
-                concepto = "dia_ausencia"
-            # Realizar la lógica de aprobación aquí
-            aprobar_dias(empleado_sol, fecha, concepto, empleado)
+            solicitudes = request.form.getlist('solicitudes')
+            for solicitud in solicitudes:
+                empleado_sol, fecha, concepto = solicitud.split('|')
+                
+                if concepto == 'Home Office':
+                    concepto = 'dia_home'
+                elif concepto == 'Día de estudio':
+                    concepto = "dia_estudio"
+                elif concepto == 'Vacaciones':
+                    concepto = "dia_vacaciones"
+                elif concepto == 'Ausencia':
+                    concepto = "dia_ausencia"
+                
+                aprobar_dias(empleado_sol, fecha, concepto, empleado)
+
             # Redirigir a una página de confirmación o a la misma página de origen
             return redirect(url_for('aprobacion.aprobacion'))
         else:
-            # Manejar el caso en que la solicitud no sea POST
             return "Método no permitido", 405
 
     # Se crea la ruta resources
